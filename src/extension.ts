806f1cb0615157266f57b7cbe8a0f2eda09a58dc
JosephAllen <joseph_m_allen@msn.com>
2020-04-22 15:48:18 -0400

    Updated Package

--- src/extension.ts
+++ src/extension.ts
@@ -14,16 +14,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "anonapex" is now active!');
+	console.log('Congratulations, your extension "apexrunner" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('anonapex.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('apexrunner.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from anonapex!');
+		vscode.window.showInformationMessage('Hello World from apexrunner!');
 	});
 
 	context.subscriptions.push(disposable);
