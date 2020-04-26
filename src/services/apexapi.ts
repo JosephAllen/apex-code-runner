e7fc58a81bf5500553c181f960d1e52d8fe38a08
JosephAllen <joseph_m_allen@msn.com>
2020-04-25 22:53:23 -0400

    Updated indents

--- src/services/apexapi.ts
+++ src/services/apexapi.ts
@@ -6,90 +6,90 @@ import { parseEnvelope } from '../parsers';
 import { Channel } from './channel';
 
 function source() {
-  const editor = vscode.window.activeTextEditor;
-  if (editor) {
-    return editor.document.getText();
-  }
-  return '';
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        return editor.document.getText();
+    }
+    return '';
 }
 export async function executeAnonymous(): Promise<void> {
-  //TODO: see https://github.com/microsoft/vscode-extension-samples/tree/master/progress-sample for progress
-  const authInfo = JSON.parse('' + process.env.APXR_AUTH_INFO);
-  const accessToken = authInfo.accessToken;
-  const instanceUrl = authInfo.instanceUrl;
-  const version = authInfo.version;
-  //const categories = getCategories();
-  let env = {
-    'soap:Envelope': {
-      $: {
-        "xmlns:soap": "http://schemas.xmlsoap.org/soap/envelope/",
-        "xmlns": "http://soap.sforce.com/2006/08/apex"
-      },
-      'soap:Header': {
-        DebuggingHeader: {
-          categories: getCategories()
+    //TODO: see https://github.com/microsoft/vscode-extension-samples/tree/master/progress-sample for progress
+    const authInfo = JSON.parse('' + process.env.APXR_AUTH_INFO);
+    const accessToken = authInfo.accessToken;
+    const instanceUrl = authInfo.instanceUrl;
+    const version = authInfo.version;
+    //const categories = getCategories();
+    let env = {
+        'soap:Envelope': {
+            $: {
+                "xmlns:soap": "http://schemas.xmlsoap.org/soap/envelope/",
+                "xmlns": "http://soap.sforce.com/2006/08/apex"
+            },
+            'soap:Header': {
+                DebuggingHeader: {
+                    categories: getCategories()
+                },
+                SessionHeader: {
+                    sessionId: accessToken
+                }
+            },
+            'soap:Body': {
+                executeAnonymous: {
+                    String: source()
+                }
+            }
+        }
+    };
+    let builder = new xml2js.Builder();
+    let requestBody = builder.buildObject(env);
+    fetch(instanceUrl + '/services/Soap/s/' + version, {
+        method: 'post',
+        body: requestBody,
+        headers: {
+            'Content-Type': 'text/xml;charset=UTF-8',
+            'SOAPAction': '""'
         },
-        SessionHeader: {
-          sessionId: accessToken
-        }
-      },
-      'soap:Body': {
-        executeAnonymous: {
-          String: source()
-        }
-      }
-    }
-  };
-  let builder = new xml2js.Builder();
-  let requestBody = builder.buildObject(env);
-  fetch(instanceUrl + '/services/Soap/s/' + version, {
-    method: 'post',
-    body: requestBody,
-    headers: {
-      'Content-Type': 'text/xml;charset=UTF-8',
-      'SOAPAction': '""'
-    },
-  }).then((res) => res.text()).then((response) => {
-    parseXml(response);
-  });
+    }).then((res) => res.text()).then((response) => {
+        parseXml(response);
+    });
 }
 
 function getCategories() {
-  let categories = [];
-  const configOptions = vscode.workspace.getConfiguration('apex-code-runner').logCategoryOptions;
-  for (const [key, value] of Object.entries(configOptions)) {
-    categories.push({ category: key, level: value });
-  }
-  return categories;
+    let categories = [];
+    const configOptions = vscode.workspace.getConfiguration('apex-code-runner').logCategoryOptions;
+    for (const [key, value] of Object.entries(configOptions)) {
+        categories.push({ category: key, level: value });
+    }
+    return categories;
 }
 
 function parseXml(xml: string) {
-  const stripNS = xml2js.processors.stripPrefix;
-  const parseOptions = {
-    emptyTag: null,
-    explicitArray: false,
-    ignoreAttrs: true,
-    normalizeTags: false,
-    tagNameProcessors: [stripNS]
-  };
-  xml2js.parseString(xml, parseOptions, function (err, result) {
-    const env = Object.assign({}, result.Envelope);
-    const message = parseEnvelope(env);
-    let channel = Channel.getInstance();
-    if (err) {
-      const errMsg = err.toString();
-      channel.writeDebugLog(errMsg);
-      channel.writeUserLog(errMsg);
-      channel.showLog();
-      return;
-    }
-    channel.writeUserLog(message);
-    if (env.Header.DebuggingInfo.debugLog !== null) {
-      channel.writeDebugLog('DEBUG LOG\n' + env.Header.DebuggingInfo.debugLog);
-    }
-    else {
-      channel.writeDebugLog(message);
-    }
-    channel.showLog();
-  });
+    const stripNS = xml2js.processors.stripPrefix;
+    const parseOptions = {
+        emptyTag: null,
+        explicitArray: false,
+        ignoreAttrs: true,
+        normalizeTags: false,
+        tagNameProcessors: [stripNS]
+    };
+    xml2js.parseString(xml, parseOptions, function (err, result) {
+        const env = Object.assign({}, result.Envelope);
+        const message = parseEnvelope(env);
+        let channel = Channel.getInstance();
+        if (err) {
+            const errMsg = err.toString();
+            channel.writeDebugLog(errMsg);
+            channel.writeUserLog(errMsg);
+            channel.showLog();
+            return;
+        }
+        channel.writeUserLog(message);
+        if (env.Header.DebuggingInfo.debugLog !== null) {
+            channel.writeDebugLog('DEBUG LOG\n' + env.Header.DebuggingInfo.debugLog);
+        }
+        else {
+            channel.writeDebugLog(message);
+        }
+        channel.showLog();
+    });
 }
